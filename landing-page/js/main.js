a55b5cac8f240b7ed93da32266872c29ac472e1c
GiulianoMendez <u201824528@upc.edu.pe>
2021-11-26 01:11:19 -0500

    Update

--- landing-page/js/main.js
+++ landing-page/js/main.js
@@ -1,3 +1,29 @@
+google.load("visualization", "1", {packages:["corechart"]});
+google.setOnLoadCallback(drawChart1);
+function drawChart1() {
+  var data = google.visualization.arrayToDataTable([
+    ['Año', '20-30 años', '30-60 años'],
+    ['2017',  232,       456],
+    ['2018',  258,       560],
+    ['2019',  292,       783],
+    ['2020',  210,       320],
+    ['2021',  222,       396]
+  ]);
+
+  var options = {
+    title: 'Cantidad de Buenos Ahorradores según rango de edades',
+    hAxis: {title: 'Año', titleTextStyle: {color: 'Black'}}
+ };
+
+var chart = new google.visualization.ColumnChart(document.getElementById('chart_div1'));
+  chart.draw(data, options);
+}
+
+$(window).resize(function(){
+  drawChart1();
+});
+    
+
 $('.navbar a').on('click', function (e) {
   if (this.hash !== '') {
     e.preventDefault();
@@ -12,3 +38,9 @@ $('.navbar a').on('click', function (e) {
     $(hash).addClass("active");
   }
 });
+$(window).resize(function(){drawColColors();});
+
+window.onresize = doALoadOfStuff;
+function doALoadOfStuff() {
+  drawColColors();
+}
